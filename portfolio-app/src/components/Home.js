ea53eaded0158d17c8d57675c5116854ece81b56
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-01-30 15:34:22 +0530

    Building Home component

--- portfolio-app/src/components/Home.js
+++ portfolio-app/src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { HiArrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-      
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Vibha Nimbalkar
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I'm a full-stack developer. Currently I'm focused on building
+          responsive full-stack web application.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- portfolio-app/src/components/Navbar.js
+++ portfolio-app/src/components/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
       </ul>
 
       {/* Social Icons */}
-      <div className="flex fixed flex-col top-[35%] left-0">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
