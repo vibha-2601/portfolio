a182e4b84880a669338381842ddda72c1fef6b65
vibha-2601 <vibhanimbalkar2601@gmail.com>
2023-01-31 17:59:23 +0530

    Building Work component

---
+++ portfolio-app/src/components/Work.js
@@ -0,0 +1,77 @@
+import React from "react";
+import WorkImg from "../assests/projects/workImg.jpeg";
+import realEstate from "../assests/projects/realestate.jpg";
+
+const Work = () => {
+  return (
+    // name-for react smooth scroll
+    <div name="work" className="w-full sm:h-screen text-gray-300 bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4  flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">//Checkout some of my recent work</p>
+        </div>
+
+        {/* Projects/grid container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4">
+          {/* Card item container */}
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2  bg-white text-gray-700 font-bold text-lg">
+                    code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Card item container */}
+          <div
+            style={{ backgroundImage: `url(${realEstate})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2  bg-white text-gray-700 font-bold text-lg">
+                    code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
